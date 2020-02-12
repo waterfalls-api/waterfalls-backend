9c8c479d7ad73e5e1f4ae51eef8e1fea22ed710b
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2020-02-12 15:53:24 -0600

    init

--- users/users-router.js
+++ users/users-router.js
@@ -1,14 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Users = require('./users-model.js');
-const restricted = require('../auth/restricted-middleware.js');
+const Users = require("./users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
 
-router.get('/', restricted, (req, res) => {
-    Users.find()
-        .then(users => {
-            res.json(users);
-        })
-        .catch(error =>  {console.log('----->', error); res.send(error)});
+router.get("/", restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      console.log("----->", error);
+      res.send(error);
+    });
 });
 
 module.exports = router;
