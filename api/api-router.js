71cac6ee2dda80097a7eac0a5aaab37b97d2d108
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2020-02-12 16:58:50 -0600

    setting up auth0

--- api/api-router.js
+++ api/api-router.js
@@ -1,10 +1,27 @@
+const router = require("express").Router();
 
-const router = require('express').Router();
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+const path = require("path");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const config = require("../auth/config.js");
 
-router.use('/auth', authRouter);
-router.use('/users', usersRouter);
+const authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${config.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+  audience: [config.AUDIENCE],
+  issuer: `https://${config.AUTH0_DOMAIN}/`,
+  algorithm: "RS256"
+});
+
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+
+router.use("/auth", authRouter);
+router.use("/users", usersRouter);
 
 module.exports = router;
---
+++ auth/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  clientId: "hyzJ9cAFI9aR3jFSOOsHYxTNn2OmZN34",
+  domain: "waterfalls.auth0.com",
+  redirect: "http://localhost:3000/close-popup",
+  // we will go over this redirect soon.
+  logoutUrl: "http://localhost:3000",
+  audience: "https://waterfalls-api"
+};
